a041cf0d662d0a3ddeaf54a15d9c9a4164ddd0ee
Raul Melo <melo.raulf@gmail.com>
2020-11-12 09:31:52 +0100

    fix: generate tags for all supported langs

--- src/pages/tag/[slug].tsx
+++ src/pages/tag/[slug].tsx
@@ -10,6 +10,7 @@ import {
   SocialApiData,
 } from '@types-api';
 import { TagPage, TagPageProps } from '@screens/Tag/TagPage';
+import { SupportedLanguages } from '@types-app';
 
 const Tag = ({ tag, personalInfo, social, site }: TagPageProps) => {
   return (
@@ -26,6 +27,7 @@ type Params = {
   params: {
     slug: string;
   };
+  locale: SupportedLanguages;
 };
 
 export const getStaticProps = async ({ params }: Params) => {
@@ -52,15 +54,34 @@ export const getStaticProps = async ({ params }: Params) => {
 export const getStaticPaths: GetStaticPaths = async () => {
   const tags = (await Backend.fetch('post-tags')) as PostsTagApiData;
 
-  const paths = tags.map((tag) => ({
-    params: {
-      slug: tag['slug'],
-    },
-  }));
+  const paths: Params[] = [];
+
+  /**
+   * Without this generation, when I'm in `/tag/css` for instance
+   * and want to access `/pt/tag/css`, it'll redirect to a 404 page.
+   *
+   * If I switch "fallback" to true, the problem will be fixed only locally
+   * or running in server (not in serverless) and it'll also leads in the first
+   * render of TagPage having "tag" prop as undefined.
+   *
+   * In this way I generate the same page for both lang and I don't have to do
+   * any workaround.
+   */
+
+  ['en', 'pt'].forEach((lang) => {
+    tags.forEach((tag) => {
+      paths.push({
+        params: {
+          slug: tag.slug,
+        },
+        locale: lang as SupportedLanguages,
+      });
+    });
+  });
 
   return {
     paths,
-    fallback: true,
+    fallback: false,
   };
 };
 
