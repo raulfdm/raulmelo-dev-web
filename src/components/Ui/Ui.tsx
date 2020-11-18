ccc228a101fab579798154807090d32b236aeb8b
Raul Melo <melo.raulf@gmail.com>
2020-11-18 22:03:31 +0100

    refactor: Container component

--- src/components/Ui/Ui.tsx
+++ src/components/Ui/Ui.tsx
@@ -4,16 +4,15 @@ import { motion } from 'framer-motion';
 import Link from 'next/link';
 import React from 'react';
 
-export const Container = styled(motion.div)`
-  max-width: 768px;
-  width: 100%;
-  margin: 0 auto;
-  padding: 0 16px;
-
-  ${media.greaterThan('medium')`
-    padding: 0;
-  `}
-`;
+/* TODO: fix this type */
+export const Container = (props: any) => {
+  return (
+    <motion.div
+      className="container mx-auto px-4 md:px-0 max-w-screen-md"
+      {...props}
+    />
+  );
+};
 
 export const LineDivider = styled.hr`
   margin: 2rem 0;
