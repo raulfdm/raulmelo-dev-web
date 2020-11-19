3debcde4edbc99a82ea9787580ee583974572dca
Raul Melo <melo.raulf@gmail.com>
2020-11-19 21:32:06 +0100

    refactor(filter): migrate to tailwind

--- src/screens/Home/components/Filter/Filter.tsx
+++ src/screens/Home/components/Filter/Filter.tsx
@@ -2,41 +2,7 @@ import React from 'react';
 import { defineMessage } from 'react-intl';
 import { motion } from 'framer-motion';
 
-import { styled } from '@styles/styled';
 import { useLocalization } from '@hooks/useLocalization';
-
-const FilterList = styled.ul`
-  display: grid;
-  grid-gap: 20px;
-  grid-template-columns: repeat(3, max-content);
-
-  && {
-    border-bottom: 1px solid ${({ theme }) => theme.color.border};
-    margin-bottom: 30px;
-  }
-`;
-
-const FilterItem = styled(motion.li)`
-  font-size: 14px;
-  font-family: ${({ theme }) => theme.font.contentSans};
-  color: ${({ theme }) => theme.color.fontLight};
-  cursor: pointer;
-
-  p {
-    border: none;
-    padding-bottom: 8px;
-    transition: border-width 0.6s linear;
-  }
-
-  p.active {
-    color: ${({ theme }) => theme.color?.font};
-    border-bottom: 1px solid ${({ theme }) => theme.color?.font};
-  }
-
-  &:hover {
-    color: ${({ theme }) => theme.color.font};
-  }
-`;
 
 type Filters = 'all' | 'single' | 'series';
 
@@ -70,6 +36,9 @@ const variants = {
   active: {
     scale: 1.1,
   },
+  normal: {
+    scale: 1.0,
+  },
 };
 
 export const Filter: React.FC<FilterProps> = ({
@@ -79,22 +48,36 @@ export const Filter: React.FC<FilterProps> = ({
   const { formatMessage } = useLocalization();
 
   return (
-    <FilterList data-testid="postFilter">
+    <ul
+      className="flex mb-5 border-b border-gray-400 dark:border-gray-600 border-opacity-20"
+      data-testid="postFilter"
+    >
       {filters.map(({ name, message }) => {
+        const isActiveFilter = activeFilter === name;
         return (
-          <FilterItem
+          <motion.li
             key={name}
             onClick={() => changeFilter(name)}
             variants={variants}
-            whileHover={activeFilter !== name ? 'active' : ''}
+            animate={isActiveFilter ? 'active' : 'normal'}
+            whileHover={{
+              scale: 1.1,
+            }}
             data-testid={`postFilter__${name}`}
+            className="text-sm text-gray-400 cursor-pointer font-sans mr-5"
           >
-            <p className={activeFilter === name ? 'active' : ''}>
+            <p
+              className={`text-current pb-2 border-0 transition-all duration-100 ease-linear ${
+                activeFilter === name
+                  ? `border-b border-current text-gray-700 dark:text-gray-50 font-bold`
+                  : ''
+              }`}
+            >
               {formatMessage(message)}
             </p>
-          </FilterItem>
+          </motion.li>
         );
       })}
-    </FilterList>
+    </ul>
   );
 };
