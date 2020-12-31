1992ecf42282b9400c776ce4596344b2bb3e2288
Raul Melo <melo.raulf@gmail.com>
2020-12-31 09:15:27 +0100

    refactor: replace bunch of internal comps with external lib

--- src/components/MdxComponents/index.ts
+++ src/components/MdxComponents/index.ts
@@ -1,19 +1,32 @@
-import { Gif } from './Gif';
-import { BigQuote } from './BigQuote';
-import { YouTubeVideo } from './YouTubeVideo';
-import { DotDivider } from './DotDivider';
-import * as Headings from './Headings';
-import { CodePen } from './CodePen';
+import {
+  BigQuote,
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  CodePenIframe,
+  Gif,
+  YouTubeIframe,
+  DotDivider,
+} from '@raulfdm/blog-components';
+
 import { TwitterCard } from './TwitterCard';
 import { Image } from './Image';
 
 export const mdxComponents = {
   BigQuote,
   Gif,
-  CodePen,
-  YouTubeVideo,
+  CodePen: CodePenIframe,
+  YouTubeVideo: YouTubeIframe,
   hr: DotDivider,
   Image,
   TwitterCard,
-  ...Headings,
+  h1: H1,
+  h2: H2,
+  h3: H3,
+  h4: H4,
+  h5: H5,
+  h6: H6,
 };
